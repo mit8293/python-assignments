118334540487ddb3e6e5737796b5da5b25450606
Pronit <mitktdesai@gmail.com>
2022-12-13 15:22:02 +0530

    Create treasure_island.py
    Project 1

---
+++ treasure_island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input("You\'re at the crossroad, where do you want to go? Type 'left' or 'right'.\n").lower()
+if choice1 == "left":
+  #continue the game
+  choice2 = input("You\'ve come to a lake where is an island in middle of the lake. Type 'wait' to wait for boat. Type 'swim' to swim across.\n").lower()
+  if choice2 == 'wait':
+    #continue the game
+    choice3 = input("You have arrived unharmed. There is a house with 3 doors. Red, Yellow and Green\n").lower()
+    if choice3 == 'red':
+      print("Burnt in Hell. game Over")
+    elif choice3 == 'yellow':
+      print("You Win. You Cracked me")
+    elif choice3 == 'green':
+      print("You get snakes.")
+    else:
+      print("You are oversmart. You win nothing.")
+  else:
+    print("Crocodile killed you. Game Over")
+else:
+  print("You fell into a hole. Game Over.")
+  
