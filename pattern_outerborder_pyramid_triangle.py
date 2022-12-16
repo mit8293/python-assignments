7a1e3766b304459f4a7045081da245a7f2445a70
Pronit <mitktdesai@gmail.com>
2022-12-16 21:23:34 +0530

    Update pattern_outerborder_pyramid_triangle.py
    with using a single loop

--- pattern_outerborder_pyramid_triangle.py
+++ pattern_outerborder_pyramid_triangle.py
@@ -18,3 +18,12 @@ for i in range(n):
         else:
             print(" ",end = "")
     print()
+
+# for single loop
+n=6
+for i in range(1,n):
+    if i == 1:
+        print("#"*(n-i),"*"*(i))
+    if i >= 2:
+        print("#"*(n-i),"*"+"#"*(i*2-3),end="*")
+        print()
