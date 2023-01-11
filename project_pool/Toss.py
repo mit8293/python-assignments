83bc3fe7645ad1025d0c0a95cf9a315d20ebee0e
Pronit <mitktdesai@gmail.com>
2023-01-11 19:00:07 +0530

    Moved to project repo

--- project_pool/Toss.py
+++
@@ -1,9 +0,0 @@
-#Remember to use the random module
-#Hint: Remember to import the random module here at the top of the file. 🎲
-	 
-import random
-random_side = random.randint(0,1)
-if random_side == 1:
-    print("Heads")
-else:
-    print("Tails")
--- project_pool/banker_roulette.py
+++
@@ -1,8 +0,0 @@
-# Import the random module here
-import random
-# Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-
-shuffle = random.shuffle(names)
-print(f"{names[0]} is going to buy the meal today!")
